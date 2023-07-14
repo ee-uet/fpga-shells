ff1b44f9c8917f07b17fd72bf1c6b2327f450716
tamood <tayyeb.mahmood@gmail.com>
2023-07-14 23:12:06 +0900

    Added Arty A7-100T support

---
+++ arty/srcs/top.v
@@ -0,0 +1,278 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/09/2023 07:49:50 PM
+// Design Name: 
+// Module Name: top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+`define DRAM                         1
+
+module top(
+input CLK100MHZ,
+input ck_rst,
+
+output [13:0]ddr3_sdram_addr,
+  output [2:0]ddr3_sdram_ba,
+  output ddr3_sdram_cas_n,
+  output [0:0]ddr3_sdram_ck_n,
+  output [0:0]ddr3_sdram_ck_p,
+  output [0:0]ddr3_sdram_cke,
+  output [0:0]ddr3_sdram_cs_n,
+  output [1:0]ddr3_sdram_dm,
+  inout [15:0]ddr3_sdram_dq,
+  inout [1:0]ddr3_sdram_dqs_n,
+  inout [1:0]ddr3_sdram_dqs_p,
+  output [0:0]ddr3_sdram_odt,
+  output ddr3_sdram_ras_n,
+  output ddr3_sdram_reset_n,
+  output ddr3_sdram_we_n,
+    
+input uart_txd_in,
+output uart_rxd_out
+    );
+
+   wire clk_20;
+   wire clk_200;
+   wire rst_n;
+   wire fb;
+
+   PLLE2_BASE #(
+      .BANDWIDTH("OPTIMIZED"),  // OPTIMIZED, HIGH, LOW
+      .CLKFBOUT_MULT(8),        // Multiply value for all CLKOUT, (2-64)
+      .CLKFBOUT_PHASE(0.0),     // Phase offset in degrees of CLKFB, (-360.000-360.000).
+      .CLKIN1_PERIOD(10.0),      // Input clock period in ns to ps resolution (i.e. 33.333 is 30 MHz).
+      // CLKOUT0_DIVIDE - CLKOUT5_DIVIDE: Divide amount for each CLKOUT (1-128)
+      .CLKOUT0_DIVIDE(4),
+      .CLKOUT1_DIVIDE(40),
+      .CLKOUT2_DIVIDE(1),
+      .CLKOUT3_DIVIDE(1),
+      .CLKOUT4_DIVIDE(1),
+      .CLKOUT5_DIVIDE(1),
+      // CLKOUT0_DUTY_CYCLE - CLKOUT5_DUTY_CYCLE: Duty cycle for each CLKOUT (0.001-0.999).
+      .CLKOUT0_DUTY_CYCLE(0.5),
+      .CLKOUT1_DUTY_CYCLE(0.5),
+      .CLKOUT2_DUTY_CYCLE(0.5),
+      .CLKOUT3_DUTY_CYCLE(0.5),
+      .CLKOUT4_DUTY_CYCLE(0.5),
+      .CLKOUT5_DUTY_CYCLE(0.5),
+      // CLKOUT0_PHASE - CLKOUT5_PHASE: Phase offset for each CLKOUT (-360.000-360.000).
+      .CLKOUT0_PHASE(0.0),
+      .CLKOUT1_PHASE(0.0),
+      .CLKOUT2_PHASE(0.0),
+      .CLKOUT3_PHASE(0.0),
+      .CLKOUT4_PHASE(0.0),
+      .CLKOUT5_PHASE(0.0),
+      .DIVCLK_DIVIDE(1),        // Master division value, (1-56)
+      .REF_JITTER1(0.0),        // Reference input jitter in UI, (0.000-0.999).
+      .STARTUP_WAIT("TRUE")    // Delay DONE until PLL Locks, ("TRUE"/"FALSE")
+   )
+   PLLE2_BASE_inst (
+      // Clock Outputs: 1-bit (each) output: User configurable clock outputs
+      .CLKOUT0(clk_200),   // 1-bit output: CLKOUT0
+      .CLKOUT1(clk_20),   // 1-bit output: CLKOUT0
+      // Feedback Clocks: 1-bit (each) output: Clock feedback ports
+      .CLKFBOUT(fb), // 1-bit output: Feedback clock
+      .LOCKED(rst_n),     // 1-bit output: LOCK
+      .CLKIN1(CLK100MHZ),     // 1-bit input: Input clock
+      // Control Ports: 1-bit (each) input: PLL control ports
+      .PWRDWN(PWRDWN),     // 1-bit input: Power-down
+      .RST(~ck_rst),           // 1-bit input: Reset
+      // Feedback Clocks: 1-bit (each) input: Clock feedback ports
+      .CLKFBIN(fb)    // 1-bit input: Feedback clock
+   );
+
+   // End of PLLE2_BASE_inst instantiation
+
+wire wb_stb_i;
+wire wb_we_i;
+wire [31:0]    wb_adr_i;
+wire [127:0]    wb_dat_i;
+wire wb_ack_o;
+wire [127:0]   wb_dat_o;
+
+soc_top st(
+
+    .rst_n(rst_n),                  // reset
+    .clk(clk_20),                     // clock
+    
+`ifdef DRAM
+    // DDR memory interface
+    .valid_o(wb_stb_i),
+    .we_o(wb_we_i),
+    .addr_o(wb_adr_i),
+    .data_o(wb_dat_i),
+    .ready_i(wb_ack_o),
+    .data_i(wb_dat_o),
+`endif
+
+    .irq_ext_i(0),
+    .irq_soft_i(0),
+    
+    // Uart interface IO signals
+    .uart_rxd_i(uart_txd_in),
+    .uart_txd_o(uart_rxd_out)
+);   
+
+  wire [31:0]m_axi_araddr;
+  wire [1:0]m_axi_arburst;
+  wire [3:0]m_axi_arcache;
+  wire [0:0]m_axi_arid;
+  wire [7:0]m_axi_arlen;
+  wire [0:0]m_axi_arlock;
+  wire [2:0]m_axi_arprot;
+  wire [3:0]m_axi_arqos;
+  wire m_axi_arready;
+  wire [3:0]m_axi_arregion;
+  wire [2:0]m_axi_arsize;
+  wire m_axi_arvalid;
+  wire [31:0]m_axi_awaddr;
+  wire [1:0]m_axi_awburst;
+  wire [3:0]m_axi_awcache;
+  wire [0:0]m_axi_awid;
+  wire [7:0]m_axi_awlen;
+  wire [0:0]m_axi_awlock;
+  wire [2:0]m_axi_awprot;
+  wire [3:0]m_axi_awqos;
+  wire m_axi_awready;
+  wire [3:0]m_axi_awregion;
+  wire [2:0]m_axi_awsize;
+  wire m_axi_awvalid;
+  wire [0:0]m_axi_bid;
+  wire m_axi_bready;
+  wire [1:0]m_axi_bresp;
+  wire m_axi_bvalid;
+  wire [127:0]m_axi_rdata;
+  wire [0:0]m_axi_rid;
+  wire m_axi_rlast;
+  wire m_axi_rready;
+  wire [1:0]m_axi_rresp;
+  wire m_axi_rvalid;
+  wire [127:0]m_axi_wdata;
+  wire m_axi_wlast;
+  wire m_axi_wready;
+  wire [15:0]m_axi_wstrb;
+  wire m_axi_wvalid;
+
+  arty_shell shell
+       (.clk(clk_20),
+       .sys_clk_i(clk_200),
+       
+       .ddr3_sdram_addr(ddr3_sdram_addr),
+        .ddr3_sdram_ba(ddr3_sdram_ba),
+        .ddr3_sdram_cas_n(ddr3_sdram_cas_n),
+        .ddr3_sdram_ck_n(ddr3_sdram_ck_n),
+        .ddr3_sdram_ck_p(ddr3_sdram_ck_p),
+        .ddr3_sdram_cke(ddr3_sdram_cke),
+        .ddr3_sdram_cs_n(ddr3_sdram_cs_n),
+        .ddr3_sdram_dm(ddr3_sdram_dm),
+        .ddr3_sdram_dq(ddr3_sdram_dq),
+        .ddr3_sdram_dqs_n(ddr3_sdram_dqs_n),
+        .ddr3_sdram_dqs_p(ddr3_sdram_dqs_p),
+        .ddr3_sdram_odt(ddr3_sdram_odt),
+        .ddr3_sdram_ras_n(ddr3_sdram_ras_n),
+        .ddr3_sdram_reset_n(ddr3_sdram_reset_n),
+        .ddr3_sdram_we_n(ddr3_sdram_we_n),
+       
+        .m_axi_araddr(m_axi_araddr),
+        .m_axi_arburst(m_axi_arburst),
+        .m_axi_arcache(m_axi_arcache),
+        .m_axi_arid(m_axi_arid),
+        .m_axi_arlen(m_axi_arlen),
+        .m_axi_arlock(m_axi_arlock),
+        .m_axi_arprot(m_axi_arprot),
+        .m_axi_arqos(m_axi_arqos),
+        .m_axi_arready(m_axi_arready),
+        //.m_axi_arregion(m_axi_arregion),
+        .m_axi_arsize(m_axi_arsize),
+        .m_axi_arvalid(m_axi_arvalid),
+        .m_axi_awaddr(m_axi_awaddr),
+        .m_axi_awburst(m_axi_awburst),
+        .m_axi_awcache(m_axi_awcache),
+        .m_axi_awid(m_axi_awid),
+        .m_axi_awlen(m_axi_awlen),
+        .m_axi_awlock(m_axi_awlock),
+        .m_axi_awprot(m_axi_awprot),
+        .m_axi_awqos(m_axi_awqos),
+        .m_axi_awready(m_axi_awready),
+        //.m_axi_awregion(m_axi_awregion),
+        .m_axi_awsize(m_axi_awsize),
+        .m_axi_awvalid(m_axi_awvalid),
+        .m_axi_bid(m_axi_bid),
+        .m_axi_bready(m_axi_bready),
+        .m_axi_bresp(m_axi_bresp),
+        .m_axi_bvalid(m_axi_bvalid),
+        .m_axi_rdata(m_axi_rdata),
+        .m_axi_rid(m_axi_rid),
+        .m_axi_rlast(m_axi_rlast),
+        .m_axi_rready(m_axi_rready),
+        .m_axi_rresp(m_axi_rresp),
+        .m_axi_rvalid(m_axi_rvalid),
+        .m_axi_wdata(m_axi_wdata),
+        .m_axi_wlast(m_axi_wlast),
+        .m_axi_wready(m_axi_wready),
+        .m_axi_wstrb(m_axi_wstrb),
+        .m_axi_wvalid(m_axi_wvalid),
+        .rst_n(rst_n));
+    
+wb2axi  adapter
+(
+.clk(clk_20),
+.rst_n(rst_n),
+
+.wb_stb_i(wb_stb_i),
+.wb_we_i(wb_we_i),
+.wb_adr_i(wb_adr_i),
+.wb_dat_i(wb_dat_i),
+.wb_ack_o(wb_ack_o),
+.wb_dat_o(wb_dat_o),
+
+// AXI signals
+        .m_axi_araddr(m_axi_araddr),
+        .m_axi_arburst(m_axi_arburst),
+        .m_axi_arcache(m_axi_arcache),
+        .m_axi_arid(m_axi_arid),
+        .m_axi_arlen(m_axi_arlen),
+        .m_axi_arprot(m_axi_arprot),
+        .m_axi_arqos(m_axi_arqos),
+        .m_axi_arready(m_axi_arready),
+        .m_axi_arsize(m_axi_arsize),
+        .m_axi_arvalid(m_axi_arvalid),
+        .m_axi_awaddr(m_axi_awaddr),
+        .m_axi_awburst(m_axi_awburst),
+        .m_axi_awcache(m_axi_awcache),
+        .m_axi_awid(m_axi_awid),
+        .m_axi_awlen(m_axi_awlen),
+        .m_axi_awprot(m_axi_awprot),
+        .m_axi_awqos(m_axi_awqos),
+        .m_axi_awready(m_axi_awready),
+        .m_axi_awsize(m_axi_awsize),
+        .m_axi_awvalid(m_axi_awvalid),
+        .m_axi_bid(m_axi_bid),
+        .m_axi_bready(m_axi_bready),
+        .m_axi_bresp(m_axi_bresp),
+        .m_axi_bvalid(m_axi_bvalid),
+        .m_axi_rdata(m_axi_rdata),
+        .m_axi_rid(m_axi_rid),
+        .m_axi_rlast(m_axi_rlast),
+        .m_axi_rready(m_axi_rready),
+        .m_axi_rresp(m_axi_rresp),
+        .m_axi_rvalid(m_axi_rvalid),
+        .m_axi_wdata(m_axi_wdata),
+        .m_axi_wlast(m_axi_wlast),
+        .m_axi_wready(m_axi_wready),
+        .m_axi_wstrb(m_axi_wstrb),
+        .m_axi_wvalid(m_axi_wvalid));    
+endmodule
